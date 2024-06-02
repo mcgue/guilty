de90fd28a0e931d175c7ce48202f87e3888f1760
mcgue <mc8@protonmail.com>
2024-06-02 10:08:10 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# guilty
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/mcgue/guilty)
---
+++ main.py
@@ -0,0 +1,6 @@
+# Run `python main.py` in the terminal
+
+# Note: Python is lazy loaded so the first run will take a moment,
+# But after cached, subsequent loads are super fast! ⚡️
+
+print ("Guilty " * 34)
